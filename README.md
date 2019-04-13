0d633ec5e5510175b2a2c7737f38d9b46978c51e
Michael Tobias <axis139@gmail.com>
2019-04-12 17:41:32 -0700

    adds IAM role to service

---
+++ README.md
@@ -0,0 +1,3 @@
+### Helpful links
+https://serverless.com/framework/docs/providers/aws/events/sqs/
+https://serverless.com/framework/docs/providers/aws/events/sqs/
--- server-functions/post-vote/handler.js
+++ server-functions/post-vote/handler.js
@@ -4,29 +4,31 @@ const AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
 
 module.exports.postVote= async (event, context) => {
-  let sqs = AWS.SQS({apiVersion: '2012-11-05'});
-  let urlparams = {
-    QueueName: 'voting-app-queue',
-    // QueueOwnerAwsAccountId: ''
-  };
+  let sqs = new AWS.SQS({apiVersion: '2012-11-05'});
   let voteQueueUrl;
+  let sqsMessageResponse;
 
-
+  console.log(event)
   try {
-    voteQueueUrl = await sqs.getQueueUrl(urlparams).promise();
-
-
+    voteQueueUrl = await sqs.getQueueUrl({
+        QueueName: 'voting-app-queue',
+    }).promise();
+    sqsMessageResponse = await sqs.sendMessage({
+      MessageBody: JSON.stringify(event),
+      QueueUrl: voteQueueUrl.QueueUrl
+    }).promise()
   }
   catch (err) {
     return {
-      statusCode: 400,
+      statusCode: 404,
       body: JSON.stringify(err)
     }
   }
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      voteQueueUrl,
+      sqsMessageResponse,
       input: event,
     }),
   };
